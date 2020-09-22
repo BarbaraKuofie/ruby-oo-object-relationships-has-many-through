d127142008776c9ea4dfa29c2344554327074f79
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-09-22 07:19:41 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,44 @@
 class Customer
-  
+  attr_accessor :name, :age 
+
+    @@all = []
+    
+    def initialize(name, age)
+        @name = name 
+        @gae = age 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    ## this is to create a new meal 
+    ## we don't need to pass in a customer because we're passing in self 
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end 
+
+## this is a way for the custeomer and waiter to get information about each other 
+# custermer instance will look at all the meals and select the ones that belong to them
+## meals belong to...  
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self 
+        end 
+    end 
+
+## this will tell the customer who their waiters are for all meals
+## reusing meals method, since we can isolate the meals to the customer 
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end 
+    end 
+
+# to get this customer's last waiter 
+    def last_waiter 
+        self.waiters.last.name 
+    end 
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,51 @@
 class Waiter
+    attr_accessor :name, :yrs_experience 
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name 
+        @yrs_experience = yrs_experience
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+# this is to create a new meal for the waiter
+# since we're in the waiter class we can pass in an instance of itself as the waiter
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    ## this is to find the meals where the waiter is this waiter instance 
+    def meals 
+        Meal.all.select do |meal|
+            meal.waiter == self 
+        end 
+    end 
+
+# this is to find the customer who tips the best 
+    def best_tipper 
+       best_tipped_meal = meals.max do |meal_a, meal_b|
+        meal_a.tip <=> meal_b.tip
+        end 
+        best_tipped_meal.customer
+    end 
+
+    # another way of writing the above 
+    # def best_tipper
+    #meals.max{|meal_a, meal_b| meal_a.tip <=> meal_b.tip}.customer
+
+## this is to find the most frequent customer 
+    def most_frequent_customer 
+        counter = self.meals.customer.count 
+        Meal.all.sort_by{|meals|meals.customer.counter}.last 
+    end 
+
+    def worst_tipper 
+        meals.min{|meal_a, meal_b| meal_a.tip <=> meal_b.tip}.customer 
+    end
 
 end
